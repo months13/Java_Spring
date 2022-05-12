c224a71a2422b6647ac92708e395be63182b632b
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-12 14:24:15 +0900

    update

--- ch2/src/main/java/com/fastcampus/ch2/MyDate.java
+++ ch2/src/main/java/com/fastcampus/ch2/MyDate.java
@@ -3,6 +3,24 @@ package com.fastcampus.ch2;
 public class MyDate {
 	
 	private int year;
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
 	private int month;
 	private int day;
 	
--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC5.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC5.java
@@ -4,33 +4,42 @@ import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class YoilTellerMVC5 {
-    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
-    public String main(int year, int month, int day, Model model) { // 작업 결과를 model에 저장한다.
- 
-        // 1. 유효성 검사 메서드 따로 빼서 정리.
-    	if(!isValid(year, month, day)) 
-    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    @ExceptionHandler(Exception.class)
+	public String catcher(Exception ex) {
+		System.out.println("ex="+ex);
+		
+		return "yoilError";
+	}
+    
+    @RequestMapping("/getYoilMVC5") // http://localhost/ch2/getYoilMVC5?year=2021&month=10&day=1
+//  public String main(@ModelAttribute("myDate") MyDate date, Model m) { // 아래와 동일 
+    public String main(@ModelAttribute MyDate date, Model m) { // @ModelAttribute사용, 반환 타입은 String  
+System.out.println("myDate="+date);
+
+    	// 1. 유효성 검사 
+    	if(!isValid(date))
+    		return "yoilError";
     	
         // 2. 처리
-    	char yoil = getYoil(year, month, day);
+    	char yoil = getYoil(date);
 
-        // 3. Model에 작업 결과 저장 . view가 출력할 때 사용한다.
-        model.addAttribute("year", year);
-        model.addAttribute("month", month);
-        model.addAttribute("day", day);
-        model.addAttribute("yoil", yoil);
+    	// 3. Model에 작업한 결과를 저장 
+        // @ModelAttribute 덕분에 MyDate를 저장안해도 됨. View로 자동 전달됨.
+//      m.addAttribute("myDate", date);     	
+//      m.addAttribute("yoil", yoil);        
         
-        // 4. 작업 결과를 보여줄 View의 이름을 반환
-        return "yoil"; // /WEB-INF/views/yoil.jsp 설정 파일에 저장되어있다.
-        // 여기서 반환타입을 void로 설정하면 매핑된 URL에 의해 뷰의 이름이 결정된다.
-        // 또, ModelAndView로 반환타입으로 설정 시 모델과 뷰를 합칠수있다. Model을 안 줘도 됨 이렇게 하면면
-        // ModelAndView타입이 정의되어있음.
-        // 다양하게 구현할 수 있음에 집중하자.
+      	// 4. 작업 결과를 보여줄 뷰의 이름을 반환  
+      	return "yoil";
+    }
+    
+    private @ModelAttribute("yoil") char getYoil(MyDate date) {
+    	return getYoil(date.getYear(), date.getMonth(), date.getDay());
     }
     
     private char getYoil(int year, int month, int day) {
@@ -40,12 +49,15 @@ public class YoilTellerMVC5 {
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         return " 일월화수목금토".charAt(dayOfWeek);
     }
+
+    private boolean isValid(MyDate date) {
+    	return isValid(date.getYear(), date.getMonth(), date.getDay());
+    }
     
     private boolean isValid(int year, int month, int day) {    
     	if(year==-1 || month==-1 || day==-1) 
     		return false;
     	
-    	return (1<=month && month<=12) && (1<=day && day<=31); // 제대로 된 지 확인.
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
     }
-    
 }
