f7e84228bc2fd8cab2a41c25c269773e042291a0
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-18 23:57:24 +0900

    4 update

--- ch4/ch4/src/main/java/com/fastcampus/ch4/controller/Main.java
+++ ch4/ch4/src/main/java/com/fastcampus/ch4/controller/Main.java
@@ -1,54 +1,45 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static char[][] map;
-    static boolean[][][][] visit;
-
-    static int[] dr = {-1,1,0,0};
-    static int[] dc = {0,0,-1,1};
-
-    public static void main(String args[]) throws IOException {
+    static int N,m,M,T,R;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st=  new StringTokenizer(br.readLine());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
-        visit = new boolean[N][M][N][M];
+        int count = 0;
+        int time = 0;
+        int current = m;
 
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
-            for(int j=0; j<M; j++){
-
+        while(count<N) {
+            if(current+T <= M) {
+                count++;
+                time++;
+                current += T;
+            }else {
+                if((current - R)< m) {
+                    time++;
+                    current = m ;
+                }else {
+                    time++;
+                    current -= R;
+                }
             }
+
+            if(current + T > M && current == m) {
+                System.out.println("-1");
+                return;
+            }
+
         }
-
-
-
-
+        System.out.println(time);
     }
 }
 
-class Marble{
-    int red_r;
-    int red_c;
-
-    int blue_r;
-    int blue_c;
-
-    int cnt;
-
-    public Marble(int red_r, int red_c, int blue_r, int blue_c, int cnt) {
-        this.red_r = red_r;
-        this.red_c = red_c;
-        this.blue_r = blue_r;
-        this.blue_c = blue_c;
-        this.cnt = cnt;
-    }
-
-}
